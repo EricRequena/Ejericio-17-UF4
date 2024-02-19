da2db1a0536ba52dce15e3d45e2633413ff2c023
eric <eric.requena.7e7@itb.cat>
2024-02-19 13:43:21 +0100

    Add project files.

---
+++ Ejericio 17 UF4/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejericio_17_UF4
+{
+    public class Car
+    {
+        private int gasolina;
+        public Car(int gasolina)
+        {
+            this.gasolina = gasolina;
+        }
+        public void Drive()
+        {
+            if (gasolina > 0)
+            {
+                Console.WriteLine("Conducir");
+            }
+        }
+        public bool Refuel(int gasolina)
+        {
+            this.gasolina += gasolina;
+            return true;
+        }
+    }
+}
---
+++ Ejericio 17 UF4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ejericio_17_UF4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Introduce la cantidad de gasolina que quieres repostar");
+            int gasolina = Convert.ToInt32(Console.ReadLine());
+            Car coche = new Car(0);
+            coche.Refuel(gasolina);
+            coche.Drive();
+        }
+    }
+}
